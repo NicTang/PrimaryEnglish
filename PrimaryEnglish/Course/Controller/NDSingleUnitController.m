4e29a0f45e6754456ceba1f05caf4c8302f0b851
Nic-Downey <329517313@qq.com>
2016-07-14 11:05:07 +0800

    删除了注释

--- PrimaryEnglish/Course/Controller/NDSingleUnitController.m
+++ PrimaryEnglish/Course/Controller/NDSingleUnitController.m
@@ -17,35 +17,5 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
-
-//- (void)requestData
-//{
-//    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-//    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
-//    //POST请求
-////    NSDictionary *dict = @{@"id":self.courseID};
-//    [manager POST:KCourseDetailUrlString parameters:dict progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-//    
-//        NSDictionary *infoDict = responseObject[@"pkgInfo"];
-////        NDInfoModel *model = [[NDInfoModel alloc]init];
-////        [model setValuesForKeysWithDictionary:infoDict];
-//        //        NSLog(@"model:%@",model);
-////        self.model = model;
-////        self.navigationItem.title = model.title;
-//        
-//        NSArray *array = responseObject[@"senceList"];
-//        self.cellDataArray = [NDDetailModel arrayOfModelsFromDictionaries:array];
-//        //刷新cell
-//        [self.tableView reloadData];
-//        
-//        //        NSLog(@"responseObject:%@",responseObject);
-//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-//        if (error) {
-//            NSLog(@"error :%@",error.localizedDescription);
-//        }
-//    }];
-//}
-
 @end
